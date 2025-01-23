2185aae9c84568409421ada7b05361081e401c02
Ikerdas13 <ikergg130007@gmail.com>
2025-01-23 13:19:45 +0100

    sum_ab -Version 1

---
+++ Sum/sum_ab.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+
+int main()
+{
+int numero1 = 0;
+int numero2 = 0;
+
+std::cout<<"Enter a number >=1:" << std::endl;
+std::cin >> numero1;
+
+std::cout<<"Enter b number >=1:" << std::endl;
+std::cin >> numero2;
+
+std::cout<<"the sum from " << numero1 << " to " << numero2 << " is" <<std::endl;
+}
