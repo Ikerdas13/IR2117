3b51e3ee9b560e3f6d619923dc81bf7cb2ed6994
Ikerdas13 <ikergg130007@gmail.com>
2025-01-23 13:00:10 +0100

    sum -Versin 1

---
+++ Sum/sum.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+int numero = 0;
+int rdo= 0;
+std::cout<<"Enter a number:" << std::endl;
+
+std::cout<<"The sum from 1 to " << numero << " is " << rdo <<std::endl;
+return 0;
+}
