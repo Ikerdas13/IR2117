20438bd6025ac554c12514ac098d5f6034ad8692
Ikerdas13 <ikergg130007@gmail.com>
2025-01-23 13:07:48 +0100

    sum -Version 4

--- Sum/sum.cc
+++ Sum/sum.cc
@@ -7,14 +7,17 @@ int numero = 0;
 int rdo= 0;
 std::cout<<"Enter a number:" << std::endl;
 std::cin >> numero;
+
+if (numero>=1){
 for(int i=1; i <= numero; i++) {
 rdo += i;
 }
 std::cout<<"The sum from 1 to " << numero << " is " << rdo <<std::endl;
 }
-
-
-
+else{
+std::cout<<"Invalid number" << std::endl;
+}
+}
 
 
 
