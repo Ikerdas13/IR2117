6d25b2f023884a8e621e54768d4b2ee9dbb24f49
Íker Giménez García <al435577@aula0106aco.sg.uji.es>
2025-01-24 17:47:19 +0100

    sum_ab -Version 2

--- Sum/sum_ab.cc
+++ Sum/sum_ab.cc
@@ -6,12 +6,25 @@ int main()
 {
 int numero1 = 0;
 int numero2 = 0;
+int rdo = 0;
 
 std::cout<<"Enter a number >=1:" << std::endl;
 std::cin >> numero1;
 
 std::cout<<"Enter b number >=1:" << std::endl;
 std::cin >> numero2;
-
-std::cout<<"the sum from " << numero1 << " to " << numero2 << " is" <<std::endl;
+if(numero1>=1){
+for(int i=numero1; i<=numero2; i++){
+rdo+=i;
+}
+std::cout<<"the sum from " << numero1 << " to " << numero2 << " is " << rdo << std::endl;
+}
+else{
+std::cout<<"Enter a number >=1:" << std::endl;
+std::cin >> numero1;
+for(int i=numero1; i<=numero2; i++){
+rdo+=i;
+}
+std::cout<<"the sum from " << numero1 << " to " << numero2 << " is " << rdo << std::endl;
+}
 }
