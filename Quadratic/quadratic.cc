500be423902abe1c92bce38210a72c0793c67e07
Íker Giménez García <al435577@aula0122aco.sg.uji.es>
2025-01-27 22:17:28 +0100

    quadratic -Version 1

---
+++ Quadratic/quadratic.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+cout <<"Introduce a:" <<endl;
+int a;
+cin >> a;
+
+cout <<"Introduce b:" <<endl;
+int b;
+cin >> b;
+
+cout <<"Introduce c:" <<endl;
+int c;
+cin >> c;
+
+
+cout << "The equacion have "<<a<<"x² "<<b<<"x "<< c<<endl;
+}
