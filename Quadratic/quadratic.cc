ce68a73f97699dfee7c6d65cbd0912ccdfe83d0a
Íker Giménez García <al435577@aula0122aco.sg.uji.es>
2025-01-27 22:30:52 +0100

    quadratic -Version 3

--- Quadratic/quadratic.cc
+++ Quadratic/quadratic.cc
@@ -4,26 +4,23 @@
 using namespace std;
 
 int main() {
-    cout << "Versión 2: Resolver el caso general (discriminante >= 0)\n";
-
+ cout << "Versión 3: Discriminante negativo (mensaje)\n";
     double a, b, c;
     cout << "Introduce los coeficientes a, b y c: ";
     cin >> a >> b >> c;
 
     if (a == 0) {
         cout << "Esto no es una ecuación cuadrática.\n";
-        return 1; // Devuelve un valor de error
+        return 0;
     }
 
     double discriminante = b * b - 4 * a * c;
-    if (discriminante >= 0) {
-        double x1 = (-b + sqrt(discriminante)) / (2 * a);
-        double x2 = (-b - sqrt(discriminante)) / (2 * a);
-        cout << "Las soluciones son: " << x1 << " y " << x2 << "\n";
-    } else {
+    if (discriminante < 0) {
         cout << "El discriminante es negativo. No hay soluciones reales.\n";
+    } else {
+        cout << "El discriminante no es negativo. Continuar con la siguiente versión.\n";
     }
 
-    return 0; 
+return 0; 
 }
 
