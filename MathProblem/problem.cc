a637b77d6fc9bffca66ae016983ed116829b31d1
Ikerdas13 <ikergg1307@gmail.com>
2025-02-03 18:47:58 +0100

    problem -Vesión 4

--- MathProblem/problem.cc
+++ MathProblem/problem.cc
@@ -4,25 +4,33 @@ using namespace std;
 int main() {
     int red_balls, blue_balls;
 
-    // Get user input for the number of red and blue balls
     cout << "Enter the number of red balls: ";
     cin >> red_balls;
     cout << "Enter the number of blue balls: ";
     cin >> blue_balls;
 
-    // Total number of balls
+
     int total_balls = red_balls + blue_balls;
 
-    // Handle edge cases
+  
     if (total_balls == 0) {
         cout << "Error: The total number of balls cannot be zero." << endl;
     } else {
-        // Calculate the probability of drawing a red ball
-        double probability_red = static_cast<double>(red_balls) / total_balls;
+        
+        double probability_first_red = static_cast<double>(red_balls) / total_balls;
 
-        // Output the result
-        cout << "The probability of drawing a red ball is: " << probability_red << endl;
+        
+        int remaining_red_balls = red_balls - 1;
+        int remaining_total_balls = total_balls - 1;
+
+        
+        double probability_second_red = static_cast<double>(remaining_red_balls) / remaining_total_balls;
+
+        
+        cout << "The probability of drawing a red ball on the first draw is: " << probability_first_red << endl;
+        cout << "The probability of drawing a red ball on the second draw (after a red ball was drawn first) is: " << probability_second_red << endl;
     }
 
     return 0;
 }
+
