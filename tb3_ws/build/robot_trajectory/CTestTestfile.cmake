4b61aaf60f94b63a1de569f9e3b041b4e2392a60
Ikerdas13 <ikergg1307@gmail.com>
2025-02-11 11:53:00 +0100

    Commit de publisher

---
+++ tb3_ws/build/robot_trajectory/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/usuario/IR2117/tb3_ws/src/robot_trajectory
+# Build directory: /home/usuario/IR2117/tb3_ws/build/robot_trajectory
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
