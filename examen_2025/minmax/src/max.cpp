2ec7b754a285a5e08ffa8ffb7f96a8724174c1af
Ikerdas13 <ikergg1307@gmail.com>
2025-02-24 09:58:23 +0100

    minmax -Version7

--- examen_2025/minmax/src/max.cpp
+++ examen_2025/minmax/src/max.cpp
@@ -12,29 +12,32 @@ void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
   
     numeros.push_back(msg->data);
-    int min;
+    int max;
+    int longitud = numeros.size();
     std_msgs::msg::Int32 out_msg;
     
     
      std::sort(numeros.begin(), numeros.end());
-	min = numeros[0];
-    std::cout << min << std::endl;
+     	
+	max = numeros[longitud - 1];
+	
+   
     
-   out_msg.data = min;  
+   out_msg.data = max;  
    publisher->publish(out_msg); 
 }
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("min");
+    auto node = rclcpp::Node::make_shared("max");
 
    
     auto subscription = node->create_subscription<std_msgs::msg::Int32>(
         "number", 10, topic_callback); 
         
        
-   publisher = node->create_publisher<std_msgs::msg::Int32>("min", 10);  
+   publisher = node->create_publisher<std_msgs::msg::Int32>("max", 10);  
  
 
    
