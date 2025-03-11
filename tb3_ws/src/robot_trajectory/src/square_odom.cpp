2b14bd14a018ddaa55ecbe354ddbaae7034b1925
Ikerdas13 <ikergg1307@gmail.com>
2025-03-11 13:16:32 +0100

    versionj casi final square_odom

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -1,5 +1,5 @@
 #include <chrono>
-#include "rclcpp/rclcpp.hpp" 
+#include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include <cmath>
 #include "geometry_msgs/msg/twist.hpp"
@@ -11,10 +11,16 @@ double y_ini;
 double theta_ini;
 
 double x;
-double y; 
+double y;
 double theta;
 
-double calculate_yaw(nav_msgs::msg::Odometry message) {
+double normalizar_angle(double angle) {
+    while (angle > M_PI) angle -= 2.0 * M_PI;
+    while (angle < -M_PI) angle += 2.0 * M_PI;
+    return angle;
+}
+
+double calculate_yaw(const nav_msgs::msg::Odometry& message) {
     double qw = message.pose.pose.orientation.w;
     double qx = message.pose.pose.orientation.x;
     double qy = message.pose.pose.orientation.y;
@@ -22,13 +28,13 @@ double calculate_yaw(nav_msgs::msg::Odometry message) {
 
     double siny_cosp = 2 * (qw * qz + qx * qy);
     double cosy_cosp = 1 - 2 * (qy * qy + qz * qz);
-    return std::atan2(siny_cosp, cosy_cosp); 
+    return std::atan2(siny_cosp, cosy_cosp);
 }
 
-void topic_callback(nav_msgs::msg::Odometry message) {
-    x = message.pose.pose.position.x;
-    y = message.pose.pose.position.y;
-    theta = calculate_yaw(message);
+void topic_callback(const nav_msgs::msg::Odometry::SharedPtr message) {
+    x = message->pose.pose.position.x;
+    y = message->pose.pose.position.y;
+    theta = calculate_yaw(*message);
 }
 
 int main(int argc, char *argv[]) {
@@ -36,69 +42,63 @@ int main(int argc, char *argv[]) {
     auto node = rclcpp::Node::make_shared("square_odom");
     auto subscriber = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
     auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    
-    
+
     node->declare_parameter("linear_speed", 0.1);
     node->declare_parameter("angular_speed", 0.1);
-    node->declare_parameter("square_length", 1);
+    node->declare_parameter("square_length", 1.0);
     double target_angle = M_PI / 2;
-    
+
     double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
     double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
-    int square_length = node->get_parameter("square_length").get_parameter_value().get<int>();
-
-
-     
+    double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
 
     rclcpp::WallRate loop_rate(10ms);
 
     geometry_msgs::msg::Twist msg;
 
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+
     for (int j = 0; j < 4; j++) {
-        // Avanzar
         float distance_travelled = 0.0;
         x_ini = x;
         y_ini = y;
         theta_ini = theta;
 
         while (rclcpp::ok() && distance_travelled < square_length) {
-            float delta_x = x - x_ini;
-            float delta_y = y - y_ini;
-            distance_travelled = std::sqrt(delta_x * delta_x + delta_y * delta_y);
-            msg.linear.x = linear_speed;  
-            msg.angular.z = 0.0;  
+            msg.linear.x = linear_speed;
+            msg.angular.z = 0.0;
             publisher->publish(msg);
-            rclcpp::spin_some(node); 
+
+            distance_travelled = std::sqrt(std::pow(x - x_ini, 2) + std::pow(y - y_ini, 2));
+
+            rclcpp::spin_some(node);
             loop_rate.sleep();
         }
 
         msg.linear.x = 0.0;
         msg.angular.z = 0.0;
         publisher->publish(msg);
-        rclcpp::spin_some(node); 
+
+        rclcpp::spin_some(node);
         loop_rate.sleep();
 
-       
-        float theta_ini = theta; 
-        while (rclcpp::ok() && theta-theta_ini < target_angle) {
-         
-
-            
-            msg.linear.x = 0.0;  
-            msg.angular.z = angular_speed;  
+        theta_ini = theta;
+        while (rclcpp::ok() && std::abs(normalizar_angle(theta - theta_ini)) < target_angle) {
+            msg.linear.x = 0.0;
+            msg.angular.z = angular_speed;
             publisher->publish(msg);
-            rclcpp::spin_some(node); 
+
+            rclcpp::spin_some(node);
             loop_rate.sleep();
         }
 
-        
         msg.linear.x = 0.0;
         msg.angular.z = 0.0;
         publisher->publish(msg);
-        rclcpp::spin_some(node); 
-        loop_rate.sleep();
 
-        
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
     }
 
     rclcpp::shutdown();
