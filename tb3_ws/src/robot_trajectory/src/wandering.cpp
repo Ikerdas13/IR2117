0eba7e71da4daa96a65e8536992eecfa64f222c8
Ikerdas13 <ikergg1307@gmail.com>
2025-03-18 11:43:22 +0100

    wandering -Versión 1

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -1,26 +1,41 @@
-
 #include <chrono>
-#include "rclcpp/rclcpp.hpp" 
+#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
 using namespace std::chrono_literals;
 
-int main(int argc, char *argv[])
-{
-
-rclcpp::init(argc, argv);
-auto node = rclcpp::Node::make_shared ("publisher");
-auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10); std_msgs::msg::String message;
-auto publish_count = 0;
-rclcpp::WallRate loop_rate (500ms);
-
-
-while (rclcpp::ok()) {
-message.data = "Hello, world!" + std::to_string (publish_count++);
-publisher->publish (message); 
-rclcpp:: spin_some (node); 
-loop_rate.sleep();
+void callback_topic(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+    std::cout << "LaserScan received" << std::endl;
 }
-rclcpp:: shutdown();
-return 0;
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+
+    // Nodo para suscripción
+    auto subscriber_node = rclcpp::Node::make_shared("subscriber");
+    auto subscription = subscriber_node->create_subscription<sensor_msgs::msg::LaserScan>(
+        "scan", 10, callback_topic);
+
+    // Nodo para publicación
+    auto publisher_node = rclcpp::Node::make_shared("publisher");
+    auto publisher = publisher_node->create_publisher<std_msgs::msg::String>("cmd_vel", 10);
+    std_msgs::msg::String message;
+
+    rclcpp::WallRate loop_rate(10ms);
+
+    // Bucle de publicación
+    while (rclcpp::ok()) {
+        message.data = "0";  
+        publisher->publish(message);
+        loop_rate.sleep();
+    }
+
+    // Espera para el procesamiento de mensajes
+    rclcpp::spin(subscriber_node);
+
+    // Cerrar el sistema correctamente
+    rclcpp::shutdown();
+    return 0;
 }
+
