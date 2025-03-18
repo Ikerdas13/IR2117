5358713ae88f53461f35bc856cd45975e4820b10
Ikerdas13 <ikergg1307@gmail.com>
2025-03-18 12:23:55 +0100

    wandering -Versión4

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include <algorithm>  // Para std::min
 
 using namespace std::chrono_literals;
 
@@ -12,25 +13,29 @@ void callback_topic(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
         std::cout << "Ranges size: " << msg->ranges.size() << std::endl;
 
         if (msg->ranges.size() > 270) {
+            float min_value = std::numeric_limits<float>::infinity();  
+
+           
             for (int i = 0; i <= 9; ++i) {
-                std::cout << "Value at index " << i << ": " << msg->ranges[i] << std::endl;
+                min_value = std::min(min_value, msg->ranges[i]);
+            }
+
+           
+            for (int i = 350; i <= 359; ++i) {
+                min_value = std::min(min_value, msg->ranges[i]);
             }
 
             
-            for (int i = 350; i <= 359; ++i) {
-                std::cout << "Value at index " << i << ": " << msg->ranges[i] << std::endl;
-            }
+            std::cout << "Minimum value in ranges[0..9] and ranges[350..359]: " << min_value << std::endl;
         } else {
             std::cout << "LaserScan data is not large enough to access these indices." << std::endl;
         }
     }
 }
 
-
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
 
-    
     auto node = rclcpp::Node::make_shared("wandering");
     auto subscriber = node->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10, callback_topic);
@@ -39,7 +44,6 @@ int main(int argc, char *argv[]) {
 
     rclcpp::WallRate loop_rate(10ms);
 
-    
     while (rclcpp::ok()) {
         message.data = "0";  
         publisher->publish(message);
@@ -47,8 +51,6 @@ int main(int argc, char *argv[]) {
         loop_rate.sleep();
     }
 
-
-   
     rclcpp::shutdown();
     return 0;
 }
